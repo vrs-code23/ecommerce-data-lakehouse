0d735987adb17c78cd1e4cc0c56b10b6f6018524
varsha <varsha2362003@gmail.com>
2026-06-18 17:40:20 +0530

    Delete analysis directory

--- analysis/mongodb_test.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from urllib.parse import quote_plus
-username=quote_plus("varsha_admin")
-password=quote_plus("Tanishk@2005")
-
-uri = f"mongodb+srv://varsha_admin:Tanishk@2005@cluster0.aozvrql.mongodb.net/?appName=Cluster0"
-
-print("Connected Successfully!")
-
